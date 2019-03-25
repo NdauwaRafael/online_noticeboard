d8c8d46f43220e9e240cb4b74dc548f89f85d0c2
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-25 22:57:26 +0300

    add student leader and super administrator to the roles file

--- noticeboard/noticeboard/roles.py
+++ noticeboard/noticeboard/roles.py
@@ -6,3 +6,19 @@ class DepartmentHead(AbstractUserRole):
     available_permissions = {
         'create_departmental_notices': True,
     }
+
+
+# Student Leader
+class StudentLeader(AbstractUserRole):
+    available_permissions = {
+        'create_student_notices': True,
+    }
+
+
+# Student Leader
+class SuperAdmistrator(AbstractUserRole):
+    available_permissions = {
+        'create_student_notices': True,
+        'create_departmental_notices': True,
+        'create_public_notices': True
+    }
