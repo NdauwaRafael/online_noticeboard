211d5517162ace1968fdad9d8c65224bd0b74cb3
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-25 22:58:36 +0300

    include permissions in the settings file

--- noticeboard/noticeboard/settings.py
+++ noticeboard/noticeboard/settings.py
@@ -52,6 +52,8 @@ REST_FRAMEWORK = {
 
 AUTH_USER_MODEL = 'accounts.User'
 
+ROLEPERMISSIONS_MODULE = 'noticeboard.roles'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
