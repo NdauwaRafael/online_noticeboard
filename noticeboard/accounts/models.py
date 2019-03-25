20f82f1bbb325945af062ad12bc3824ebe57b6ad
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-25 15:45:01 +0300

    update accounts model .

--- noticeboard/accounts/models.py
+++ noticeboard/accounts/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# from departments.models import Department
-# from roles.models import Role
+from departments.models import Department
+from roles.models import Role
 
 
 # Create your models here.
 class User(AbstractUser):
     registration_no = models.CharField(max_length=100, unique=True)
-    # department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
-    # role_id = models.ForeignKey(Role, on_delete=models.CASCADE)
+    department_id = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
+    role_id = models.ForeignKey(Role, on_delete=models.CASCADE, null=True)
     bio = models.CharField(max_length=400)
