f8c9c198cdc1013c5058f8a4ebe5323af6ed01b8
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-26 23:21:32 +0300

    a default for field category

--- noticeboard/posts/models.py
+++ noticeboard/posts/models.py
@@ -2,10 +2,11 @@ from django.db import models
 # from django.contrib.auth.models import User
 from django.conf import settings
 
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=500)
-    category = models.CharField(max_length=100, unique=True)
+    category = models.CharField(max_length=100, default='public')
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
