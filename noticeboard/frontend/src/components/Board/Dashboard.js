df3bf8668362ac67142420d71b03951c7f49690f
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-27 00:10:48 +0300

    remove add post form from dashboard

--- noticeboard/frontend/src/components/Board/Dashboard.js
+++ noticeboard/frontend/src/components/Board/Dashboard.js
@@ -1,12 +1,10 @@
 import React, { Component, Fragment } from 'react';
 import Posts from './Post/Posts';
-import AddPost from './Post/Form';
 class Dashboard extends Component {
     render() {
         return (
             <Fragment>
                 <h1>Bord Dashboard</h1>
-                <AddPost />
                 <Posts />
             </Fragment>
         )
