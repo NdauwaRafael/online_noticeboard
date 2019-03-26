4175f53b42c77d76aa7aeb9d3554cb3d16923a9e
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-26 23:22:56 +0300

    remove unused class meta

--- noticeboard/accounts/models.py
+++ noticeboard/accounts/models.py
@@ -10,14 +10,3 @@ class User(AbstractUser):
     department_id = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
     role_id = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='user_role', null=True)
     bio = models.CharField(max_length=400, null=True)
-
-
-class Meta:
-    permissions = (("can_go_in_non_ac_bus", "To provide non-AC Bus facility"),
-                   ("can_go_in_ac_bus", "To provide AC-Bus facility"),
-                   ("can_stay_ac-room", "To provide staying at AC room"),
-                   ("can_stay_ac-room", "To provide staying at Non-AC room"),
-                   ("can_go_dehradoon", "Trip to Dehradoon"),
-                   ("can_go_mussoorie", "Trip to Mussoorie"),
-                   ("can_go_haridwaar", "Trip to Haridwaar"),
-                   ("can_go_rishikesh", "Trip to Rishikesh"))
