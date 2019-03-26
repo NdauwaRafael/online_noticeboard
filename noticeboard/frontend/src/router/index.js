f87b648122571d3356c905ed56863d3596472f84
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-27 00:09:50 +0300

    add route to add post form and also make an admin route

--- noticeboard/frontend/src/router/index.js
+++ noticeboard/frontend/src/router/index.js
@@ -3,15 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 
 //ROUTES
 import Dashboard from '../components/Board/Dashboard';
+import AddPost from '../components/Board/Post/Form'
 import Register from '../components/auth/Register';
 import Login from '../components/auth/Login';
 
 //PRIVATE ROUTE
 import PrivateRoute from './PrivateRoute';
+import AdminRoute from './AdminRoute'
 
 export default (
     <Switch>
         <PrivateRoute path="/" exact component={Dashboard} />
+        <AdminRoute path="/addpost" exact component={AddPost} />
         <Route path="/login" exact component={Login} />
         <Route path="/register" exact component={Register} />
     </Switch>
