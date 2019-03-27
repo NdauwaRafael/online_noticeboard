0ed59061f75f7fac53749cae86ed4ad378ad57dc
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-28 00:35:30 +0300

    add basic boiler plates for departments action file

---
+++ noticeboard/frontend/src/Redux/actions/departments/index.js
@@ -0,0 +1,16 @@
+import {
+    GET_DEPARTMENTS_SUCCESS,
+    GET_DEPARTMENTS_FAILED
+} from '../../constants/actionTypes';
+
+export const getDepartmentsSuccess = (resp)=>{
+
+};
+
+export const getDepartmentsFailed = (resp)=>{
+
+};
+
+export const getDepartments = ()=>dispatch=>{
+
+};
