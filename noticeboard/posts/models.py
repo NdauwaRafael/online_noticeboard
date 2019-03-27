d5de67bfc5e9ec75c65a323d2761f66c440235b3
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-28 00:27:58 +0300

    add department optionto in post model

--- noticeboard/posts/models.py
+++ noticeboard/posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 # from django.contrib.auth.models import User
 from django.conf import settings
+from departments.models import Department
 
 
 # Create your models here.
@@ -9,4 +10,5 @@ class Post(models.Model):
     description = models.CharField(max_length=500)
     category = models.CharField(max_length=100, default='public')
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE, null=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
