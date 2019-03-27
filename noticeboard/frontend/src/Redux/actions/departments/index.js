f7e3e6b50c0ca82e9e29a953b8504715d076119f
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-28 00:43:28 +0300

    finaloze creating departments action get method

--- noticeboard/frontend/src/Redux/actions/departments/index.js
+++ noticeboard/frontend/src/Redux/actions/departments/index.js
@@ -3,14 +3,30 @@ import {
     GET_DEPARTMENTS_FAILED
 } from '../../constants/actionTypes';
 
-export const getDepartmentsSuccess = (resp)=>{
+import * as departmentsApi from '../../constants/API/departments';
 
+import {tokenConfig} from '../auth';
+
+export const getDepartmentsSuccess = (resp) => {
+    return {
+        type: GET_DEPARTMENTS_SUCCESS,
+        departments: resp
+    }
 };
 
-export const getDepartmentsFailed = (resp)=>{
-
+export const getDepartmentsFailed = (error) => {
+    return {
+        type: GET_DEPARTMENTS_FAILED,
+        departments: error
+    }
 };
 
-export const getDepartments = ()=>dispatch=>{
-
+export const getDepartments = () => (dispatch, getState)  => {
+    departmentsApi.getDepartmentsApi(tokenConfig(getState))
+        .then(resp=>{
+            return dispatch(getDepartmentsSuccess(resp.data))
+        })
+        .catch(error=>{
+            return dispatch(getDepartmentsFailed(error.response.data))
+        })
 };
