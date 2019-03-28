f048cd934e78a5132ce9785887f675ea324587de
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-29 01:22:32 +0300

    Add delete user action

--- noticeboard/frontend/src/Redux/actions/users/index.js
+++ noticeboard/frontend/src/Redux/actions/users/index.js
@@ -4,10 +4,12 @@ import {
     ADD_USER_SUCCESS,
     ADD_USER_FAILED,
     UPDATE_USER_SUCCESS,
-    UPDATE_USER_FAILED
+    UPDATE_USER_FAILED,
+    DELETE_USER_SUCCESS
 } from '../../constants/actionTypes';
 import * as userAPI from '../../constants/API/users';
 import {tokenConfig} from "../auth";
+import {getErrors, getMessages} from "../messages";
 
 //GET
 export const getUsersSuccess = (resp) => {
@@ -72,19 +74,41 @@ export const addUser = (user) => (dispatch, getState) => {
             })
             .catch(error => {
                 console.log(error);
-                if(error.response){
+                if (error.response) {
                     return dispatch(updateUserFailed(error.response.data))
                 }
             })
     } else {
         userAPI.addUserApi(user, tokenConfig(getState))
             .then(resp => {
-                return dispatch(addUsersSuccess(resp.data))
+                return dispatch([
+                    getMessages('User was updated successfully!!'),
+                    addUsersSuccess(resp.data)
+                ])
             })
             .catch(error => {
-                if(error.response){
+                if (error.response) {
                     return dispatch(addUsersFailed(error.response.data))
                 }
             })
     }
 };
+
+//DELETE
+export const deleteUserSuccess = (resp) => {
+    return {
+        type: DELETE_USER_SUCCESS
+    }
+};
+
+export const deleteUser = (id) => (dispatch, getState) => {
+    userAPI.deleteUserApi(id, tokenConfig(getState))
+        .then(resp => {
+            return dispatch([
+                deleteUserSuccess(resp),
+                getMessages('User was deleted successfully!!'),])
+        })
+        .catch(error => {
+            return dispatch(getErrors('Failed to delete!!' + error.toString()))
+        })
+}
