edab33d8ba0ccac8f7784e9ecd59b6d5f12052c8
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-28 17:42:20 +0300

    add a new owner field to accounts model

--- noticeboard/accounts/models.py
+++ noticeboard/accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from departments.models import Department
 from roles.models import Role
+from django.conf import settings
 
 
 # Create your models here.
@@ -9,4 +10,5 @@ class User(AbstractUser):
     registration_no = models.CharField(max_length=100, unique=True)
     department_id = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
     role_id = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='user_role', null=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE, null=True)
     bio = models.CharField(max_length=400, null=True)
