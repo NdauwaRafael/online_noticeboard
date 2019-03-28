178ceea21f45494dee09aadbe1733b434af654bd
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-28 22:50:05 +0300

    add basic requirements for manage users component

--- noticeboard/frontend/src/components/users/MangeUsers.js
+++ noticeboard/frontend/src/components/users/MangeUsers.js
@@ -1,7 +1,32 @@
-import  React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import UsersTable from "./partials/UsersTable";
+import {bindActionCreators} from "redux";
+import {getUsers} from "../../Redux/actions/users";
+import {connect} from "react-redux";
 
-class MangeUsers extends Component{
+class MangeUsers extends Component {
+    render() {
+        const {users, auth} = this.props;
+        return (
+            <Fragment>
+                <h1>All Users </h1>
+                <UsersTable users={users} auth={auth}/>
+            </Fragment>
+        )
+    }
+}
 
+const mapStateToProps = ({users: {users}, auth}) => {
+    return {
+        users,
+        auth
+    }
 };
 
-export default MangeUsers;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getUsers: bindActionCreators(getUsers, dispatch)
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MangeUsers);
