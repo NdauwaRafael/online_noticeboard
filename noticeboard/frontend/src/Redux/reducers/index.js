4656a4c423f82b5c6b2a51fcf8ea3aee3cc53448
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-03-28 21:00:14 +0300

    add roles reducer to the roor reducer file

--- noticeboard/frontend/src/Redux/reducers/index.js
+++ noticeboard/frontend/src/Redux/reducers/index.js
@@ -4,11 +4,13 @@ import errors from './errors';
 import messages from './messages';
 import auth from './auth'
 import departments from './departments';
+import roles from 'roles';
 
 export default combineReducers({
     posts,
     errors,
     messages,
     auth,
-    departments
+    departments,
+    roles
 })
