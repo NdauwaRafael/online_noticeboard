ce3ca222d138af48634ea2d03f76de7ebca78a64
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-04-01 08:43:54 +0300

    add department stateless components

---
+++ noticeboard/frontend/src/components/Departments/partials/DepartmentsForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+DepartmentsForm.propTypes = {
+    
+};
+
+function DepartmentsForm(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default DepartmentsForm;
