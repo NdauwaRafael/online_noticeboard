f5a28ae766cd30bb82b22cba33564f2beb529870
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-04-01 08:53:40 +0300

    add departments form component together with app proptypes

--- noticeboard/frontend/src/components/Departments/partials/DepartmentsForm.js
+++ noticeboard/frontend/src/components/Departments/partials/DepartmentsForm.js
@@ -1,14 +1,36 @@
 import React, {Fragment} from 'react';
-import PropTypes from 'prop-types';
+import TextInput from '../../common/form/Input';
+import PropTypes from "prop-types";
+import Textarea from "../../common/form/Textarea";
 
 DepartmentsForm.propTypes = {
-    
+    onSave: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    title: PropTypes.string.isRequired,
+    errors: PropTypes.object.isRequired
 };
 
-function DepartmentsForm(props) {
+function DepartmentsForm({onSave, onChange, title, description, errors}) {
     return (
         <Fragment>
+            <form onSubmit={onSave}>
 
+                <TextInput
+                    name='title'
+                    label="Title"
+                    value={title}
+                    error={errors.title}
+                    onChange={onChange}/>
+
+                <Textarea
+                    name="description"
+                    label="Description (Optional)"
+                    value={description}
+                    error={errors.description}
+                    onChange={onChange}/>
+
+                <button type="submit" className="btn btn-success">Submit User Data</button>
+            </form>
         </Fragment>
     );
 }
