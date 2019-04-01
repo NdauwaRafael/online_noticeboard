40579ffca1462ac3dae1d25b61f25b95f8d4ae74
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-04-01 09:34:18 +0300

    add actions for adding a department

--- noticeboard/frontend/src/Redux/actions/departments/index.js
+++ noticeboard/frontend/src/Redux/actions/departments/index.js
@@ -1,11 +1,14 @@
 import {
     GET_DEPARTMENTS_SUCCESS,
-    GET_DEPARTMENTS_FAILED
+    GET_DEPARTMENTS_FAILED,
+    ADD_DEPARTMENT_SUCCESS,
+    ADD_DEPARTMENT_FAILED
 } from '../../constants/actionTypes';
 
 import * as departmentsApi from '../../constants/API/departments';
 
 import {tokenConfig} from '../auth';
+import {getErrors, getMessages} from "../messages";
 
 export const getDepartmentsSuccess = (resp) => {
     return {
@@ -21,12 +24,48 @@ export const getDepartmentsFailed = (error) => {
     }
 };
 
-export const getDepartments = () => (dispatch, getState)  => {
+export const getDepartments = () => (dispatch, getState) => {
     departmentsApi.getDepartmentsApi(tokenConfig(getState))
-        .then(resp=>{
+        .then(resp => {
             return dispatch(getDepartmentsSuccess(resp.data))
         })
-        .catch(error=>{
+        .catch(error => {
             return dispatch(getDepartmentsFailed(error.response.data))
+        })
+};
+
+//ADD
+export const addDepartmentSuccess = (resp) => {
+    return {
+        type: ADD_DEPARTMENT_SUCCESS,
+        department: resp
+    }
+};
+
+export const addDepartmentFailed = (error) => {
+    return {
+        type: ADD_DEPARTMENT_FAILED,
+        error
+    }
+};
+
+export const addDepartment = (department) => (dispatch, getState) => {
+    departmentsApi.addDepartmentApi(department, tokenConfig(getState))
+        .then(resp => {
+            return dispatch([
+                addDepartmentSuccess(resp.data),
+                getMessages('Department Added successfully')
+            ])
+        })
+        .catch(error => {
+            if (error.response) {
+                return dispatch([
+                    addDepartmentFailed(error.response.data),
+                    getErrors('Department Add failed!')
+                ])
+            } else {
+                return dispatch(getErrors('Department Add failed!'))
+            }
+
         })
 };
