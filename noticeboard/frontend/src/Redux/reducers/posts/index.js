1e30776f1116e02ce7651e95d3a8647643e086b4
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-04-01 11:37:44 +0300

    add reducer configurations for update post

--- noticeboard/frontend/src/Redux/reducers/posts/index.js
+++ noticeboard/frontend/src/Redux/reducers/posts/index.js
@@ -3,6 +3,8 @@ import {
     ADD_POST_SUCCESS,
     DELETE_POST_SUCCESS,
     ADD_POST_FAILED,
+    UPDATE_POST_SUCCESS,
+    UPDATE_POST_FAILED
 } from '../../constants/actionTypes';
 
 const initialState = {
@@ -18,6 +20,7 @@ export default (state = initialState, action) => {
                 posts: [...state.posts, action.post]
             };
         case ADD_POST_FAILED:
+        case UPDATE_POST_FAILED:
             return {
                 ...state,
                 errors: action.errors
@@ -26,6 +29,14 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 posts: action.posts
+            };
+        case UPDATE_POST_SUCCESS:
+            return {
+                ...state,
+                posts: [
+                    ...state.posts.filter(post => post.id !== action.post.id),
+                    Object.assign({}, action.post)
+                ]
             };
 
         case DELETE_POST_SUCCESS:
