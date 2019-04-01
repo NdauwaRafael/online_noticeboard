27ae1a4770d6d2ec014cf3b75a73bcc6f40b28ca
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-04-01 10:12:48 +0300

    add the right props to list departments component

--- noticeboard/frontend/src/components/Departments/List.js
+++ noticeboard/frontend/src/components/Departments/List.js
@@ -1,18 +1,27 @@
 import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
+import DepartmentsTable from './partials/DepartmentsTable';
 
 class List extends Component {
     render() {
+        const {departments, user} = this.props;
         return (
             <Fragment>
-                
+                <DepartmentsTable
+                    user={user}
+                    departments={departments}
+                    deleteDepartment={}
+                />
             </Fragment>
         );
     }
 }
 
-function mapStateToProps(state) {
-    return {};
+function mapStateToProps({auth: {user}, departments: {departments}}) {
+    return {
+        user,
+        departments
+    };
 }
 
 export default connect(
--- noticeboard/frontend/src/components/Departments/partials/DepartmentsTable.js
+++ noticeboard/frontend/src/components/Departments/partials/DepartmentsTable.js
@@ -1,14 +1,50 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
 
-DepartmentsTable.propTypes = {
-    
-};
+DepartmentsTable.propTypes = {};
 
-function DepartmentsTable(props) {
+function DepartmentsTable({user, departments, deleteDepartment}) {
     return (
         <Fragment>
+            <table className="table">
+                <thead className="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    {
+                        user.role === "Administrator" ?
+                            <th>Actions</th>
+                            : null
+                    }
 
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    departments.map((department, index) => (
+                        <tr key={index}>
+                            <th scope="row">{index + 1}</th>
+                            <td>{department.title}</td>
+                            {
+                                user.role === 'Administrator' ?
+                                    <td style={{display: 'flex', justifyContent: 'space-between'}}>
+                                        <Link to={"/department/" + department.id} type="button" style={{marginRight: 20}}
+                                              className="btn btn-outline-success">Edit
+                                        </Link>
+                                        <button type="button" className="btn btn-outline-danger"
+                                                onClick={() => deleteDepartment(department.id)}>Delete
+                                        </button>
+                                    </td>
+                                    : null
+                            }
+
+                        </tr>
+                    ))
+                }
+
+                </tbody>
+            </table>
         </Fragment>
     );
 }
