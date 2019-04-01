b1be95b980f974f67a6e4c73389fe9796b489b53
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-04-01 10:58:47 +0300

    migrate department

--- noticeboard/departments/models.py
+++ noticeboard/departments/models.py
@@ -6,4 +6,4 @@ from django.conf import settings
 class Department(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=400, null=True)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE, null=True, blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="departments", on_delete=models.CASCADE, null=True, blank=True)
