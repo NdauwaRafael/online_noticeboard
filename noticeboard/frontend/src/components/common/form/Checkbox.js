fcbc8f09d65c877247042c6ba0ced9f809ac0953
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-04-02 11:55:09 +0300

    add a custom component checkbox

---
+++ noticeboard/frontend/src/components/common/form/Checkbox.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Checkbox.propTypes = {
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+};
+
+const Checkbox = ({name, value, label, error, onChange}) => {
+    let validatorClass = '';
+    let parentValidator = '';
+    let errorMessageValidator = ''
+    if (error.length > 0) {
+        validatorClass = 'is-invalid';
+        parentValidator = 'has-danger';
+        errorMessageValidator = 'invalid-feedback';
+    } else {
+        validatorClass = '';
+        parentValidator = '';
+        errorMessageValidator = 'text-muted';
+    }
+
+    return (
+        <div className={"form-group " + parentValidator}>
+            <div className="custom-control custom-checkbox">
+                <input
+                    type="checkbox"
+                    className={"custom-control-input" + validatorClass}
+                    id={name}
+                    value={value}
+                    name={name}
+                    ref={name}
+                    onChange={onChange}/>
+                <label className="custom-control-label" htmlFor={name}>{label}</label>
+            </div>
+            <small className={"form-text " + errorMessageValidator}>{error}</small>
+        </div>
+    );
+};
+
+export default Checkbox;
